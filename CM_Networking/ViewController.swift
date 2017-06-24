4525aedd4cd7f4ab683ab848f3d2544fa5c3a304
Artem Lyksa <artemlyksa@Artems-MacBook-Pro.local>
2017-06-24 14:21:39 +0300

    Created API requests using Alamofire.

--- CM_Networking/ViewController.swift
+++ CM_Networking/ViewController.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2017 Artem Lyksa. All rights reserved.
 //
 
+
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
     
@@ -14,11 +16,132 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alamofireDeleteRequest()
     }
     
     
+    func alamofireDeleteRequest() {
+        let todoEndpoint = "https://jsonplaceholder.typicode.com/todos/1"
+        Alamofire.request(todoEndpoint, method: .delete)
+            .responseJSON { response in
+                
+                if response.result.isSuccess == false {
+                    print("CALL FAILED")
+                    return
+                }
+                
+                print("DELETE ok")
+        }
+    }
     
-    func nativeReqestTest() {
+    func alamofirePostRequestTest()
+    {
+        let todoEndpoint = "https://jsonplaceholder.typicode.com/todos"
+        let newTodo: [String: Any] = ["title": "My FirstPost Using Alamofire", "completed" : 0, "userId" : 1]
+        
+        Alamofire.request(todoEndpoint, method: .post, parameters: newTodo, encoding: JSONEncoding.default)
+            
+            .responseJSON { response in
+                
+                if response.result.isSuccess == false {
+                    print("CALL FAILED")
+                    return
+                }
+                
+                guard let json = response.result.value as? [String: Any] else {
+                    print("error in .responseJSON")
+                    return
+                }
+                
+                guard let title = json["title"] else {
+                    print("value with key title does not exist")
+                    return
+                }
+                
+                print(title)
+                
+            }
+    }
+    
+    func alamofireGetRequestTest() {
+        let todoEndpoint = "https://jsonplaceholder.typicode.com/todos/1"
+        
+        Alamofire.request(todoEndpoint)
+            .responseJSON { response in
+                
+                if response.result.isSuccess == false {
+                    print("CALL FAILED")
+                    return
+                }
+                
+                guard let json = response.result.value as? [String: Any] else {
+                    print("error in .responseJSON")
+                    return
+                }
+                
+                guard let title = json["title"] else {
+                    print("value with key title does not exist")
+                    return
+                }
+                
+                print(title)
+                
+            }
+            .responseString { response in
+                if let error = response.result.error {
+                    print(error)
+                }
+                
+                if let value = response.result.value {
+                    print(value)
+                }
+        }
+    }
+    
+    
+    func nativeGetReqestTest() {
+        let todoEndpoint = "https://jsonplaceholder.typicode.com/todos/1"
+    
+        guard let url = URL(string: todoEndpoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: urlRequest) { (data, response, error) in
+            //Error handling
+            if let error = error {
+                print("error occurred. Error = \(error)")
+                return
+            }
+            
+            //Check if data exists
+            guard let responseData = data else {
+                print("didnot receive data")
+                return
+            }
+            
+            do {
+                guard let todo = try JSONSerialization.jsonObject(with:responseData, options: []) as? [String : Any] else {
+                    print("error when trying convert data to JSON")
+                    return
+                }
+                print ("todo = \(todo)")
+                
+            } catch {
+                print("error is nil")
+            }
+            
+        }
+        
+        task.resume()
+    }
+    
+    
+    func nativePostReqestTest() {
         let todoEndpoint = "https://jsonplaceholder.typicode.com/todos"
         
         guard let url = URL(string: todoEndpoint) else {
